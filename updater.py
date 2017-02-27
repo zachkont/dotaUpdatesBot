5acbebf094cfc4390d5dfdcb16dfebebf811ceb3
Zach Kontoulis <z.kontoulis@gmail.com>
2017-02-27 23:16:15 +0200

    Trim trailing whitespace

--- updater.py
+++ updater.py
@@ -22,11 +22,11 @@ bot = telebot.TeleBot("115117842:AAFTAQjUEjs9ZBJRqr5SxGD-UNeSYE0flq0")
 
 stdout =  open('C:\dotaUpdatesBot\stdout.txt', 'a')
 sys.stdout = stdout
-    
+
 log = open(r'C:\dotaUpdatesBot\log.txt', 'a')
-    
+
 while True:
-    
+
     try:
         blog_feed = feedparser.parse( dota_blog_rss_url )
         #print ("Blog feed status= " + blog_feed.status)
@@ -38,10 +38,10 @@ while True:
                 print ("%s no &#8230 found" %str(datetime.now()), file=log)
         else:
             blog_content = None
-    except:  
+    except:
         blog_content = None
         print ("%s Unexpected error. ECODE:1001"%str(datetime.now()), file=log)
-        
+
     #try:
     #    request = requests.get(steam_news_json_url)
     #    if request.status_code == 200:
@@ -74,9 +74,9 @@ while True:
     except:
         cyborgmatt_content = None
         print ("%s Unexpected error. ECODE:1003"%str(datetime.now()), file=log)
-    
+
     if blog_content is not None:
-        #try: 
+        #try:
         #    print (blog_feed["items"][0]["title"] + " |time: %s" %str(datetime.now()))
         #except:
         #    print ("%s MANASU. ECODE:9001"%str(datetime.now()), file=log)
@@ -92,21 +92,21 @@ while True:
                         bot.send_message(gid, '[New blog post!]({url}) \n\n *{title}* ```\n\n{content}...\n\n```'.format(url = blogpost.link, title=blogpost.title, content=blog_content), parse_mode="Markdown")
                     except:
                         print ("%s Unexpected error. ECODE:0002"%str(datetime.now()), file=log)
-                    
+
                 entry = { blogpost.title : blogpost.link }
                 with open('previousblogposts.json') as f:
                     data = json.load(f)
                 data.update(entry)
                 with open('previousblogposts.json', 'w') as f:
                     json.dump(data, f)
-    
+
     #if data is not None:
     #   jsonid = data['appnews']['newsitems'][0]['gid']
     #    jsontitle = data['appnews']['newsitems'][0]['title']
     #    jsoncontent = data['appnews']['newsitems'][0]['contents']
     #    jsoncontent_nice = jsoncontent.replace(" - ", "\n - ")
     #    jsonurl = data['appnews']['newsitems'][0]['url']
-    #    
+    #
     #    if jsonid not in loadjson("previousjasons") and "Dota 2 Update" in jsontitle:
     #        for uid in loadjson("userlist"):
     #            try:
@@ -118,14 +118,14 @@ while True:
     #                bot.send_message(gid, '*{title}*```> \nSteam news entry!\n\n{content_nice}\n\n```'.format(title=jsontitle, content_nice=jsoncontent_nice) + '[More info here]({url})'.format(url=jsonurl), parse_mode="Markdown", disable_web_page_preview=True)
     #            except:
     #                print ("%s Unexpected error. ECODE:0004"%str(datetime.now()), file=log)
-    #                
+    #
     #        entry = { jsonid : jsonurl }
     #        with open('previousjasons.json') as f:
     #            data = json.load(f)
     #        data.update(entry)
     #        with open('previousjasons.json', 'w') as f:
     #            json.dump(data, f)
-    
+
     if belve_content is not None:
         #try:
         #    print (belve_feed["items"][0]["title"] + " |time: %s" %str(datetime.now()))
@@ -133,7 +133,7 @@ while True:
         #    print ("%s MANASU. ECODE:9002"%str(datetime.now()), file=log)
         for post in belve_feed.entries:
             if post.title not in loadjson("previousbelvedere"):
-                if (("/u/SirBelvedere on" not in post.title) and ("Dota 2 Update" in post.title)): 
+                if (("/u/SirBelvedere on" not in post.title) and ("Dota 2 Update" in post.title)):
                     for uid in loadjson("userlist"):
                         try:
                             bot.send_message(uid, '[New post from SirBelvedere!]({url}) \n\n *{title}* ```\n\n```'.format(url = post.link ,title=post.title), parse_mode="Markdown")
@@ -141,7 +141,7 @@ while True:
                             print ("%s Unexpected error. ECODE:0005"%str(datetime.now()), file=log)
                     for gid in loadjson("grouplist").keys():
                         try:
-                            bot.send_message(gid, '[New post from SirBelvedere!]({url}) \n\n *{title}* ```\n\n```'.format(url = post.link ,title=post.title), parse_mode="Markdown")   
+                            bot.send_message(gid, '[New post from SirBelvedere!]({url}) \n\n *{title}* ```\n\n```'.format(url = post.link ,title=post.title), parse_mode="Markdown")
                         except:
                             print ("%s Unexpected error. ECODE:0006"%str(datetime.now()), file=log)
                     entry = { post.title : post.link }
@@ -150,7 +150,7 @@ while True:
                     data.update(entry)
                     with open('previousbelvedere.json', 'w') as f:
                         json.dump(data, f)
-                        
+
     if cyborgmatt_content is not None:
         #try:
         #    print (cyborgmatt_feed["items"][0]["title"] + " |time: %s" %str(datetime.now()))
@@ -158,7 +158,7 @@ while True:
         #    print ("%s MANASU. ECODE:9002"%str(datetime.now()), file=log)
         for post in cyborgmatt_feed.entries:
             if post.title not in loadjson("previouscyborgmatt"):
-                if (("/u/Cyborgmatt on" not in post.title) and ("Dota 2 Update" in post.title)): 
+                if (("/u/Cyborgmatt on" not in post.title) and ("Dota 2 Update" in post.title)):
                     for uid in loadjson("userlist"):
                         try:
                             bot.send_message(uid, '[New post from Cyborgmatt!]({url}) \n\n *{title}* ```\n\n```'.format(url = post.link ,title=post.title), parse_mode="Markdown")
@@ -166,7 +166,7 @@ while True:
                             print ("%s Unexpected error. ECODE:0005"%str(datetime.now()), file=log)
                     for gid in loadjson("grouplist").keys():
                         try:
-                            bot.send_message(gid, '[New post from Cyborgmatt!]({url}) \n\n *{title}* ```\n\n```'.format(url = post.link ,title=post.title), parse_mode="Markdown")   
+                            bot.send_message(gid, '[New post from Cyborgmatt!]({url}) \n\n *{title}* ```\n\n```'.format(url = post.link ,title=post.title), parse_mode="Markdown")
                         except:
                             print ("%s Unexpected error. ECODE:0006"%str(datetime.now()), file=log)
                     entry = { post.title : post.link }
@@ -175,9 +175,9 @@ while True:
                     data.update(entry)
                     with open('previouscyborgmatt.json', 'w') as f:
                         json.dump(data, f)
-    
+
     time.sleep(180)
-    
+
 try:
     bot.send_message("76932858", "Something terrible has happenned!!!!!!!!!!")
 except:
