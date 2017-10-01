cc029e0c290b137e35302f35bca7806a48ea6cda
Amedeo <amato.amedeo@gmail.com>
2017-10-02 00:56:44 +0200

    completely refactor updater.py
    Still contains some DRYs but should still be much better than before.

--- updater.py
+++ updater.py
@@ -1,225 +1,139 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 
-import time
-from datetime import datetime
-import sys
 import json
-import telebot
+import threading
 import feedparser
-from utils import loadjson
 
-from settings import BOT_TOKEN
-from settings import CRISIS_ACCOUNT
+from datetime import datetime, time
+from bs4 import BeautifulSoup
 
-
-
-print("updater started")
-
-dota_blog_rss_url = "http://blog.dota2.com/feed/"
-steam_news_json_url = ("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/"
-                       "?appid=570&maxlength=300&format=json")
-belvedere_reddit_rss_url = "https://www.reddit.com/user/SirBelvedere/.rss"
-cyborgmatt_reddit_rss_url = "https://www.reddit.com/user/Cyborgmatt/.rss"
-magesunite_reddit_rss_url = "https://www.reddit.com/user/Magesunite/.rss"
-
-bot = telebot.TeleBot(BOT_TOKEN)
-
-stdout = open('stdout.txt', 'a')
-sys.stdout = stdout
+from utils import bot, loadjson
 
 log = open('log.txt', 'a')
 
-while True:
 
-    try:
-        blog_feed = feedparser.parse(dota_blog_rss_url)
-        #print("Blog feed status= " + blog_feed.status)
-        if blog_feed.status == 200:
-            blog_content = unicode(blog_feed["items"][0]["summary"])
-            try:
-                blog_content, trash = blog_content.split('&#8230', 1)
-            except:
-                print("%s no &#8230 found" %str(datetime.now()), file=log)
+def get_rss_posts(url, error_message):
+    feed = feedparser.parse(url)
+    if feed.status == 200:  # OK
+        posts = feed.entries
+    else:
+        posts = None
+        print("{}: {}".format(datetime.now(), error_message), file=log)
+    return posts
+
+
+def get_dota2blog_posts():
+    return get_rss_posts("http://blog.dota2.com/feed/", "Dota 2 blog feed not accessible.")
+
+
+def get_reddit_user_posts(username):
+    return get_rss_posts("https://www.reddit.com/user/{}/submitted/.rss?limit=3".format(username),
+                         "Reddit feed of the user '{}' not accessible".format(username))
+
+
+def get_dota_blog_preview(text):
+    return text.split(" &#8230;", 1)[0]
+
+
+def get_reddit_preview(text):
+    return BeautifulSoup(text, "html.parser").text[:266]  # 266 is Max. length of dota blog summaries
+
+
+def get_relevant_sirbelvedere_posts():
+    posts = get_reddit_user_posts("SirBelvedere")
+    return [post for post in posts if
+            ("/u/SirBelvedere on" not in post.title)
+            and ("Dota 2 Update" in post.title)]
+
+
+def get_relevant_cyborgmatt_posts():
+    posts = get_reddit_user_posts("Cyborgmatt")
+    return [post for post in posts if
+            ("/u/Cyborgmatt on" not in post.title)
+            and ("Dota 2 Update" in post.title)]
+
+
+def get_relevant_magesunite_posts():
+    posts = get_reddit_user_posts("Magesunite")
+    return [post for post in posts if
+            ("/u/Magesunite on" not in post.title)
+            and ("Dota 2 Update" in post.title)]
+
+
+def post_is_fresh(post, filename):
+    return post.title not in loadjson(filename)
+
+
+def add_post_to_unfresh_list(post, filename):
+    f = open(filename, 'w+')
+    data = json.load(f)
+    data.update({post.title: post.link})
+    json.dump(data, f)
+    f.close()
+
+
+def notify_users_and_groups(message):
+    # Notify subscribed users
+    for user_id in loadjson("userlist"):
+        try:
+            bot.send_message(user_id, message, parse_mode="Markdown")
+        except:
+            print("{}: Message could not be sent to users".format(datetime.now()), file=log)
+
+    # Notify subscribed groups
+    for gid in loadjson("grouplist").keys():
+        try:
+            bot.send_message(gid, message, parse_mode="Markdown")
+        except:
+            print("{}: Message could not be sent to groups".format(datetime.now()), file=log)
+
+
+def notify_subscriber_about_reddit(username, posts):
+    if posts is None:
+        return
+
+    for post in posts:
+        if post_is_fresh(post, "previous{}".format(username.lower())):
+            # Compose message text
+            message_text = '[New post from {user}!]({url}) \n\n *{title}* ```\n\n{content}...\n\n```' \
+                .format(user=username, url=post.link, title=post.title, content=get_reddit_preview(post.summary))
+            notify_users_and_groups(message_text)
+            add_post_to_unfresh_list(post, "previous{}.json".format(username.lower()))
         else:
-            blog_content = None
-    except:
-        blog_content = None
-        print("%s Unexpected error. ECODE:1001"%str(datetime.now()), file=log)
+            break  # Post are ordered by date. If one is unfresh all the following ones also are.
 
-    #try:
-    #    request = requests.get(steam_news_json_url)
-    #    if request.status_code == 200:
-    #        data = request.json()
-    #    else:
-    #        data = None
-    #except:
-    #    print("%s Unexpected error. ECODE:1002"%str(datetime.now()), file=log)
 
-    try:
-        belve_feed = feedparser.parse(belvedere_reddit_rss_url)
-        #print("Belve feed status= " + belve_feed.status)
-        if belve_feed.status == 200:
-            belve_content = belve_feed["items"][0]["summary"]
-            belve_content = belve_content[1:100]
+def notify_subscriber_about_dota2blog(posts):
+    if posts is None:
+        return
+
+    for post in posts:
+        if post_is_fresh(post, "previousblogposts"):
+            # Compose message text
+            message_text = '[New blog post!]({url}) \n\n *{title}* ```\n\n{content}...\n\n```' \
+                .format(url=post.link, title=post.title, content=get_dota_blog_preview(post.summary))
+            notify_users_and_groups(message_text)
+            add_post_to_unfresh_list(post, "previousblogposts.json")
         else:
-            belve_content = None
-    except:
-        belve_content = None
-        print("%s Unexpected error. ECODE:1003"%str(datetime.now()), file=log)
+            break  # Post are ordered by date. If one is unfresh all the following ones also are.
 
-    try:
-        cyborgmatt_feed = feedparser.parse(cyborgmatt_reddit_rss_url)
-        #print("Cybrogmatt feed status= " + cyborgmatt_feed.status)
-        if cyborgmatt_feed.status == 200:
-            cyborgmatt_content = cyborgmatt_feed["items"][0]["summary"]
-            cyborgmatt_content = cyborgmatt_content[1:100]
-        else:
-            cyborgmatt_content = None
-    except:
-        cyborgmatt_content = None
-        print("%s Unexpected error. ECODE:1013"%str(datetime.now()), file=log)
 
-    try:
-        magesunite_feed = feedparser.parse(magesunite_reddit_rss_url)
-        #print("Magesunite feed status= " + magesunite_feed.status)
-        if magesunite_feed.status == 200:
-            magesunite_content = magesunite_feed["items"][0]["summary"]
-            magesunite_content = magesunite_content[1:100]
-        else:
-            magesunite_content = None
-    except:
-        magesunite_content = None
-        print("%s Unexpected error. ECODE:1023"%str(datetime.now()), file=log)
+def check_for_updates_and_notify():
+    posts = get_relevant_sirbelvedere_posts()
+    notify_subscriber_about_reddit("SirBelvedere", posts)
 
-    if blog_content is not None:
-        #try:
-        #    print(blog_feed["items"][0]["title"] + " |time: %s" %str(datetime.now()))
-        #except:
-        #    print("%s MANASU. ECODE:9001"%str(datetime.now()), file=log)
-        for blogpost in blog_feed.entries:
-            if blogpost.title not in loadjson("previousblogposts"):
-                for uid in loadjson("userlist"):
-                    try:
-                        bot.send_message(uid, '[New blog post!]({url}) \n\n *{title}* ```\n\n{content}...\n\n```'.format(url = blogpost.link, title=blogpost.title, content=blog_content), parse_mode="Markdown")
-                    except:
-                        print("%s Unexpected error. ECODE:0001"%str(datetime.now()), file=log)
-                for gid in loadjson("grouplist").keys():
-                    try:
-                        bot.send_message(gid, '[New blog post!]({url}) \n\n *{title}* ```\n\n{content}...\n\n```'.format(url = blogpost.link, title=blogpost.title, content=blog_content), parse_mode="Markdown")
-                    except:
-                        print("%s Unexpected error. ECODE:0002"%str(datetime.now()), file=log)
+    posts = get_relevant_cyborgmatt_posts()
+    notify_subscriber_about_reddit("Cyborgmatt", posts)
 
-                entry = {blogpost.title : blogpost.link}
-                with open('previousblogposts.json') as f:
-                    data = json.load(f)
-                data.update(entry)
-                with open('previousblogposts.json', 'w+') as f:
-                    json.dump(data, f)
+    posts = get_relevant_magesunite_posts()
+    notify_subscriber_about_reddit("Magesunite", posts)
 
-    #if data is not None:
-    #   jsonid = data['appnews']['newsitems'][0]['gid']
-    #    jsontitle = data['appnews']['newsitems'][0]['title']
-    #    jsoncontent = data['appnews']['newsitems'][0]['contents']
-    #    jsoncontent_nice = jsoncontent.replace(" - ", "\n - ")
-    #    jsonurl = data['appnews']['newsitems'][0]['url']
-    #
-    #    if jsonid not in loadjson("previousjasons") and "Dota 2 Update" in jsontitle:
-    #        for uid in loadjson("userlist"):
-    #            try:
-    #                bot.send_message(uid, '*{title}*```> \nSteam news entry!\n\n{content_nice}\n\n```'.format(title=jsontitle, content_nice=jsoncontent_nice) + '[More info here]({url})'.format(url=jsonurl), parse_mode="Markdown", disable_web_page_preview=True)
-    #            except:
-    #                print("%s Unexpected error. ECODE:0003 "%str(datetime.now()), file=log)
-    #        for gid in loadjson("grouplist").keys():
-    #            try:
-    #                bot.send_message(gid, '*{title}*```> \nSteam news entry!\n\n{content_nice}\n\n```'.format(title=jsontitle, content_nice=jsoncontent_nice) + '[More info here]({url})'.format(url=jsonurl), parse_mode="Markdown", disable_web_page_preview=True)
-    #            except:
-    #                print("%s Unexpected error. ECODE:0004"%str(datetime.now()), file=log)
-    #
-    #        entry = {jsonid : jsonurl}
-    #        with open('previousjasons.json') as f:
-    #            data = json.load(f)
-    #        data.update(entry)
-    #        with open('previousjasons.json', 'w') as f:
-    #            json.dump(data, f)
+    posts = get_dota2blog_posts()
+    notify_subscriber_about_dota2blog(posts)
 
-    if belve_content is not None:
-        #try:
-        #    print(belve_feed["items"][0]["title"] + " |time: %s" %str(datetime.now()))
-        #except:
-        #    print("%s MANASU. ECODE:9002"%str(datetime.now()), file=log)
-        for post in belve_feed.entries:
-            if post.title not in loadjson("previousbelvedere"):
-                if("/u/SirBelvedere on" not in post.title) and("Dota 2 Update" in post.title):
-                    for uid in loadjson("userlist"):
-                        try:
-                            bot.send_message(uid, '[New post from SirBelvedere!]({url}) \n\n *{title}* ```\n\n```'.format(url = post.link, title=post.title), parse_mode="Markdown")
-                        except:
-                            print("%s Unexpected error. ECODE:0005"%str(datetime.now()), file=log)
-                    for gid in loadjson("grouplist").keys():
-                        try:
-                            bot.send_message(gid, '[New post from SirBelvedere!]({url}) \n\n *{title}* ```\n\n```'.format(url = post.link, title=post.title), parse_mode="Markdown")
-                        except:
-                            print("%s Unexpected error. ECODE:0006"%str(datetime.now()), file=log)
-                    entry = {post.title : post.link}
-                    with open('previousbelvedere.json') as f:
-                        data = json.load(f)
-                    data.update(entry)
-                    with open('previousbelvedere.json', 'w+') as f:
-                        json.dump(data, f)
+    threading.Timer(180, check_for_updates_and_notify).start()
 
-    if magesunite_content is not None:
-        #try:
-        #    print(belve_feed["items"][0]["title"] + " |time: %s" %str(datetime.now()))
-        #except:
-        #    print("%s MANASU. ECODE:9002"%str(datetime.now()), file=log)
-        for post in magesunite_feed.entries:
-            if post.title not in loadjson("previousmagesunite"):
-                if("/u/Magesunite on" not in post.title) and("Dota 2 Update" in post.title):
-                    for uid in loadjson("userlist"):
-                        try:
-                            bot.send_message(uid, '[New post from Magesunite!]({url}) \n\n *{title}* ```\n\n```'.format(url = post.link, title=post.title), parse_mode="Markdown")
-                        except:
-                            print("%s Unexpected error. ECODE:0015"%str(datetime.now()), file=log)
-                    for gid in loadjson("grouplist").keys():
-                        try:
-                            bot.send_message(gid, '[New post from Magesunite!]({url}) \n\n *{title}* ```\n\n```'.format(url = post.link, title=post.title), parse_mode="Markdown")
-                        except:
-                            print("%s Unexpected error. ECODE:0016"%str(datetime.now()), file=log)
-                    entry = {post.title : post.link}
-                    with open('previousmagesunite.json') as f:
-                        data = json.load(f)
-                    data.update(entry)
-                    with open('previousmagesunite.json', 'w+') as f:
-                        json.dump(data, f)
 
-    if cyborgmatt_content is not None:
-        #try:
-        #    print(cyborgmatt_feed["items"][0]["title"] + " |time: %s" %str(datetime.now()))
-        #except:
-        #    print("%s MANASU. ECODE:9002"%str(datetime.now()), file=log)
-        for post in cyborgmatt_feed.entries:
-            if post.title not in loadjson("previouscyborgmatt"):
-                if("/u/Cyborgmatt on" not in post.title) and("Dota 2 Update" in post.title):
-                    for uid in loadjson("userlist"):
-                        try:
-                            bot.send_message(uid, '[New post from Cyborgmatt!]({url}) \n\n *{title}* ```\n\n```'.format(url = post.link, title=post.title), parse_mode="Markdown")
-                        except:
-                            print("%s Unexpected error. ECODE:0025"%str(datetime.now()), file=log)
-                    for gid in loadjson("grouplist").keys():
-                        try:
-                            bot.send_message(gid, '[New post from Cyborgmatt!]({url}) \n\n *{title}* ```\n\n```'.format(url = post.link, title=post.title), parse_mode="Markdown")
-                        except:
-                            print("%s Unexpected error. ECODE:0026"%str(datetime.now()), file=log)
-                    entry = {post.title : post.link}
-                    with open('previouscyborgmatt.json') as f:
-                        data = json.load(f)
-                    data.update(entry)
-                    with open('previouscyborgmatt.json', 'w+') as f:
-                        json.dump(data, f)
-
-    time.sleep(180)
-
-try:
-    bot.send_message(CRISIS_ACCOUNT, "Something terrible has happenned!!!!!!!!!!")
-except:
-    print("%s Unexpected error. ECODE:1111"%str(datetime.now()), file=log)
+check_for_updates_and_notify()
