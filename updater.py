7bf4c6256d3582f6c7fe3e52201be1431d377486
Zach Kontoulis <z.kontoulis@gmail.com>
2017-11-08 15:55:26 +0200

    Reduce sleep time for updater to 10s

--- updater.py
+++ updater.py
@@ -136,4 +136,4 @@ def check_for_updates_and_notify():
 print("{}: Checking for new posts has started!".format(datetime.now()), file=log)
 while True:
     check_for_updates_and_notify()
-    time.sleep(60)
+    time.sleep(10)
