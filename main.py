cc393405992ece1dea60456a951e99216a3b45f2
TimJentzsch <tim-jentzsch@gmx.de>
2018-03-01 20:43:43 +0100

    Add /nextupdate command
    Add the `/nextupdate` command, that evaluates when the next patch will come online.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import feedparser
 import dota2api
 import HTMLParser
 
+from datetime import datetime, timedelta
+
 from utils import intime, getCID, getContent, loadjson, addUser, deljson, bot, addBlogPostInstantView
 from settings import DOTA2API_TOKEN
 
@@ -18,9 +20,11 @@ heroes_list = heroes_list["heroes"]
 
 parser = HTMLParser.HTMLParser()
 
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
+
 
 @bot.message_handler(commands=['help'])
 def get_help(message):
@@ -35,6 +39,7 @@ def get_help(message):
                     parse_mode="Markdown",
                     disable_web_page_preview=True)
 
+
 @bot.message_handler(commands=['info'])
 def get_info(message):
     gitUrl = "https://github.com/zachkont/dotaUpdatesBot"
@@ -44,6 +49,52 @@ def get_info(message):
                     + u' for more information on what you can do or how to contribute!',
                     parse_mode="Markdown",
                     disable_web_page_preview=True)
+
+
+@bot.message_handler(commands=['nextupdate'])
+def next_update(message):
+    if intime(message):
+        time_init_update = datetime.strptime("2018-02-15", "%Y-%m-%d")
+        time_now = datetime.now()
+
+        time_next_update = time_init_update
+        time_dif = time_now - time_next_update
+        time_add = timedelta(days=14)  # 2 weeks
+
+        # calculate time to next update:
+        while time_dif.days >= time_add.days:
+            time_next_update += time_add  # set the next update 2 weeks later
+            time_dif = time_now - time_next_update  # recalculate time difference
+
+        weeks_to_update = time_dif.days / 7
+        days_to_update = time_dif.days % 7
+
+        # create message string:
+        twitter_url = "https://twitter.com/IceFrog/status/958912324030554113"
+        msg = u'According to the [new patch system]({twitter_url}), the next update should be '\
+            .format(twitter_url=twitter_url)
+
+        if weeks_to_update == 0:
+            if days_to_update == 0:
+                msg += u'*today*.'
+            elif days_to_update == 1:
+                msg += u'*tomorrow*.'
+            else:
+                msg += u'in *{days} days*.'.format(days=days_to_update)
+        else:
+            if weeks_to_update == 1:
+                msg += u'in *1 week*'
+            else:
+                msg += u'in *{weeks} weeks*'.format(weeks=weeks_to_update)
+            if days_to_update == 0:
+                msg += u'.'
+            elif days_to_update == 1:
+                msg += u' and *1 day*.'
+            else:
+                msg += u' and *{days} days*'.format(days=days_to_update)
+
+        # send the message
+        bot.send_message(getCID(message), msg, parse_mode="Markdown")
 
 
 @bot.message_handler(commands=['dotanews', 'dotanew', 'dnews', 'dnew'])
